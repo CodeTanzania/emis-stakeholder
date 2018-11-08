a70a6937a4bc5c93ee6a812aeff72dc8340698c3
lykmapipo <lallyelias87@gmail.com>
2018-11-08 19:45:26 +0300

    Initial Notification Implementation
    - Add helpers to get party distinct phone numbers
    - Add helpers to get party distinct emails
    - Add postman dependencies

---
+++ test/integration/party.notification.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+/* dependencies */
+const path = require('path');
+const _ = require('lodash');
+const { expect } = require('chai');
+const { Party } = require(path.join(__dirname, '..', '..'));
+
+
+describe('Party Notification', () => {
+
+  before((done) => {
+    Party.deleteMany(done);
+  });
+
+  let parties = Party.fake(10);
+
+  before((done) => {
+    Party.seed(parties, (error, created) => {
+      parties = created;
+      done(error, created);
+    });
+  });
+
+  it('should be able get distinct phones', (done) => {
+    Party.getPhones((error, phones) => {
+      expect(error).to.not.exist;
+      expect(phones).to.exist;
+      expect(phones).to.be.have.length.at.least(1);
+      done(error, phones);
+    });
+  });
+
+  it('should be able get distinct phones by criteria', (done) => {
+    const ids = _.map(_.take(parties, 2), '_id');
+    const criteria = { _id: { $in: ids } };
+    Party.getPhones(criteria, (error, phones) => {
+      expect(error).to.not.exist;
+      expect(phones).to.exist;
+      expect(phones).to.be.have.length(2);
+      done(error, phones);
+    });
+  });
+
+  it('should be able get distinct emails', (done) => {
+    Party.getPhones((error, emails) => {
+      expect(error).to.not.exist;
+      expect(emails).to.exist;
+      expect(emails).to.be.have.length.at.least(1);
+      done(error, emails);
+    });
+  });
+
+  it('should be able get distinct emails by criteria', (done) => {
+    const ids = _.map(_.take(parties, 2), '_id');
+    const criteria = { _id: { $in: ids } };
+    Party.getPhones(criteria, (error, emails) => {
+      expect(error).to.not.exist;
+      expect(emails).to.exist;
+      expect(emails).to.be.have.length(2);
+      done(error, emails);
+    });
+  });
+
+  after((done) => {
+    Party.deleteMany(done);
+  });
+
+});
